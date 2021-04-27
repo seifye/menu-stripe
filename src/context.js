11cfcd1e9f3d0cdf7ebb7037d8ee178eb435aa3a
Yehia Seif <yehia_seif@live.com>
2021-04-27 20:37:38 +0200

    setting the provider component to feed the children with the state

--- src/context.js
+++ src/context.js
@@ -1,2 +1,54 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+
+
+// setting the global context
+const AppContext = React.createContext();
+
+// setting the Provider
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// making a custom hook to be used in our child components (should start with "use" as well to be able to use hooks inside of it)
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
